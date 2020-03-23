f00977ca1e550425c20f421aba7082c6d2faba34
dyatchin <dyacthin@d-partners.ru>
2020-03-23 15:22:51 +0300

    Class Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,36 +2,41 @@ import React from 'react';
 import s from './Users.module.css';
 import Axios from 'axios';
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    if(props.users.length === 0){
+    constructor(props) {
+        super(props);
         Axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then( users => props.setUsers(users.data.items))
+             .then( users => props.setUsers(users.data.items))
     }
 
-    return <div> 
+    render() { 
+        return <div>
         {
-            props.users.map(u => <div className={s.mb} key={u.id}>
-                    <span>
-                        <div className={s.imgContainer}>
-                            {u.photos.small ? <img src={u.photos.small} className={s.iconUser} alt=""/> 
-                                            : <img src='./notFindPhotoUser.png' className={s.iconUser} alt=""/>}
-                        </div>
-                    </span>
+            this.props.users.map(u => <div className={s.mb} key={u.id}>
+                <span>
+                    <div className={s.imgContainer}>
+                        {u.photos.small ? <img src={u.photos.small} className={s.iconUser} alt="" />
+                        : <img src='./notFindPhotoUser.png' className={s.iconUser} alt="" />}
+                    </div>
+                </span>
 
-                    {u.followed ? <button onClick={ () => props.unfollow(u.id) }>follow</button>
-                                : <button onClick={ () => props.follow(u.id) }>unfollow</button>}
+                {u.followed ? <button onClick={ ()=> this.props.unfollow(u.id) }>follow</button>
+                : <button onClick={ ()=> this.props.follow(u.id) }>unfollow</button>}
 
-                    <span>
-                        <div className={s.mt}>
-                            <span className={s.mr}>{u.name}</span> 
-                            <span>{u.status}</span>
-                        </div> 
-                    </span>
-                </div>
+                <span>
+                    <div className={s.mt}>
+                        <span className={s.mr}>{u.name}</span>
+                        <span>{u.status}</span>
+                    </div>
+                </span>
+            </div>
             )
         }
-    </div>
+        </div>
+    }
+
 }
+
 
 export default Users;
